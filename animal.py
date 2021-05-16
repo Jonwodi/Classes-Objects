93ee1d97a6c2889d112d612444343b18927c1e1d
Jordan Onwodi <>
2021-05-16 21:36:09 +0000

    Initial commit

---
+++ animal.py
@@ -0,0 +1,14 @@
+class Animal:
+    # intialize
+  def __init__(self, name, age):
+    # instance variable
+    self.name = name
+    self.age = age
+    # instance method
+  def speak(self):
+    return f"My name is {self.name} and I'm {self.age} years old"
+
+  def object(self):
+    return f"My name is {self.name}"
+    
+
---
+++ main.py
@@ -0,0 +1,16 @@
+# import this
+
+# # from file import Klass
+# from animal import Animal
+# # instancen / object
+# dog = Animal(name ="Jay", age = 30)
+# cat = Animal(name ="Steph", age = 30)
+# print(dog.speak())
+# print(cat.speak())
+# print(dog.object())
+# print(cat.object())
+# print(Animal.__dict__)
+
+
+# print(dir(Animal))
+
